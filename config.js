5a312e641efbb6092e9f025be03b6b3fff1a409d
ozereefe <123651724+ozereefe@users.noreply.github.com>
2026-04-20 17:49:02 +0300

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,8 @@ const CONFIG = {
     anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1obGlrcm5pcGJ6aW9xenByc2N3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzY2ODk4MjYsImV4cCI6MjA5MjI2NTgyNn0.i1HlpVqd-4Gh5q5BG7Jw2aptzQqsz2blHFLyze7Fv7s',
   },
   mailchimp: {
-    // Mailchimp kurulunca buraya eklenecek
-    actionUrl: '',
+    baseUrl: 'https://github.us13.list-manage.com/subscribe/post-json',
+    u: 'a583eb5b534c05781c54014a3',
+    id: '4a00188647',
   }
 };
