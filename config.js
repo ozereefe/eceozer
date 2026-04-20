41fbc19ccf2a57d612d0e612a07afced9d37367c
ozereefe <123651724+ozereefe@users.noreply.github.com>
2026-04-20 17:41:52 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,10 @@
+const CONFIG = {
+  supabase: {
+    url: 'https://mhlikrnipbzioqzprscw.supabase.co',
+    anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1obGlrcm5pcGJ6aW9xenByc2N3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzY2ODk4MjYsImV4cCI6MjA5MjI2NTgyNn0.i1HlpVqd-4Gh5q5BG7Jw2aptzQqsz2blHFLyze7Fv7s',
+  },
+  mailchimp: {
+    // Mailchimp kurulunca buraya eklenecek
+    actionUrl: '',
+  }
+};
